0bee74ecb6aebb56f44e8f8b0163d422e074aa31
Dennis Hermsmeier <d.hermsmeier@mittwald.de>
2020-03-17 13:37:05 +0100

    use sudo

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -14,13 +14,13 @@ OPERATOR_SDK_BIN="${GENERIC_BIN_DIR}/operator-sdk"
 sudo wget -q \
     "https://github.com/operator-framework/operator-sdk/releases/download/v${OPERATOR_SDK_VERSION}/operator-sdk-v${OPERATOR_SDK_VERSION}-x86_64-linux-gnu" \
     -O "${OPERATOR_SDK_BIN}"
-chmod +x "${OPERATOR_SDK_BIN}"
+sudo chmod +x "${OPERATOR_SDK_BIN}"
 
 ## Install Helm
 export HELM_INSTALL_DIR="${GENERIC_BIN_DIR}"
 HELM_BIN="${GENERIC_BIN_DIR}/helm"
 sudo curl -sS -L https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-chmod +x ${HELM_BIN}
+sudo chmod +x ${HELM_BIN}
 
 ## Install Docker-CE
 DOCKER_BIN="${GENERIC_BIN_DIR}/docker"
