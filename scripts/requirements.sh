663dd4b7e08d0fb3dd51c39b3c5b907873336f3a
Luca Berneking <l.berneking@mittwald.de>
2021-02-22 11:01:39 +0100

    only use sudo where needed to fix helm as user (#7)

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -5,18 +5,18 @@ set -e
 GENERIC_BIN_DIR="/usr/local/bin"
 
 ## Generic stuff
-apt-get -qq update
-apt-get -qq install curl wget
+sudo apt-get -qq update
+sudo apt-get -qq install curl wget git
 
 ## Install Operator-SDK
 if [[ ! -x "$(command -v operator-sdk)" ]]; then
     OPERATOR_SDK_VERSION="0.15.2"
     OPERATOR_SDK_BIN="${GENERIC_BIN_DIR}/operator-sdk"
 
-    wget -q \
+    sudo wget -q \
         "https://github.com/operator-framework/operator-sdk/releases/download/v${OPERATOR_SDK_VERSION}/operator-sdk-v${OPERATOR_SDK_VERSION}-x86_64-linux-gnu" \
         -O "${OPERATOR_SDK_BIN}"
-    chmod +x "${OPERATOR_SDK_BIN}"
+    sudo chmod +x "${OPERATOR_SDK_BIN}"
 fi
 
 ## Install Helm
@@ -25,7 +25,7 @@ if [[ ! -x "$(command -v helm)" ]]; then
     HELM_BIN="${GENERIC_BIN_DIR}/helm"
 
     curl -sS -L https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
-    chmod +x ${HELM_BIN}
+    sudo chmod +x ${HELM_BIN}
 fi
 
 ## Install Helm push
@@ -33,5 +33,5 @@ helm plugin install https://github.com/chartmuseum/helm-push.git
 
 ## Install Docker-CE
 if [[ ! -x "$(command -v docker)" ]]; then
-    curl -sS -L https://get.docker.com/ | bash
+    curl -sS -L https://get.docker.com/ | sudo bash
 fi
